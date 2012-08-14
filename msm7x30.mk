c1a5a5c48e9cd80412b00cb0ae98b90e6770c7cc
synergy dev <synergy@codefi.re>
2012-08-14 13:57:11 -0700

    msm7x30.mk: build libcolorconvert

--- msm7x30.mk
+++ msm7x30.mk
@@ -83,7 +83,8 @@ PRODUCT_PACKAGES += \
     libmm-omxcore \
     libdivxdrmdecrypt \
     libOmxVdec \
-    libstagefrighthw
+    libstagefrighthw \
+    libI420colorconvert
 
 # Misc
 PRODUCT_PACKAGES += \
