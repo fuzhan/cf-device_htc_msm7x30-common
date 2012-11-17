16b1195fa33b2944fc05caff92c30d454923f5fb
synergydev <synergy@spentlemon.com>
2012-11-17 04:57:37 +0000

    msm7x30.mk: dont build libI420colorconvert [broken in qcom/media]

--- msm7x30.mk
+++ msm7x30.mk
@@ -80,8 +80,7 @@ PRODUCT_PACKAGES += \
     libmm-omxcore \
     libdivxdrmdecrypt \
     libOmxVdec \
-    libstagefrighthw \
-    libI420colorconvert
+    libstagefrighthw
 
 # Misc
 PRODUCT_PACKAGES += \
