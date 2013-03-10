1280f6230aad059e77102ba0305e99744b45dd7d
Nicholas Flintham <nick@flinny.org>
2013-03-10 19:41:06 +0000

    update mk file and kernel headers
    Change-Id: I8944f9d3ae90bf4f73e383fb44364d871a982837
    
    Conflicts:
    	msm7x30.mk

--- msm7x30.mk
+++ msm7x30.mk
@@ -76,7 +76,12 @@ PRODUCT_PACKAGES += \
     libmemalloc \
     liboverlay \
     libQcomUI \
-    libtilerenderer
+    libtilerenderer \
+    libdashplayer
+
+#wireless
+PRODUCT_PACKAGES += \
+    libnetcmdiface
 
 # Media
 PRODUCT_PACKAGES += \
@@ -128,5 +133,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Hack for adreno blob glitches
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.hw=1
-
+    debug.composition.type=gpu \
+    debug.sf.hw=1 \
+    debug.egl.hw=1
