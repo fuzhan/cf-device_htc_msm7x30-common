91d24627f9dac2f3da9b12f54adc05e97a6d066b
synergydev <synergy@spentlemon.com>
2013-03-13 06:36:02 +0000

    move wifi defines out of common (1/2)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,24 +36,6 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mcpu=cortex-a9 -mtune=cortex-a9 -mfloat-ab
 
 TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x30-common/include
 
-ifeq ($(BOARD_7X30_USES_COMMON_BCMDHD_DEFINES),true)
-# Merged Wifi Defines
-WIFI_BAND := 802_11_ABGN
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_WLAN_DEVICE := bcmdhd
-WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_AP := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_MODULE_NAME := bcmdhd
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcmdhd.ko"
-BOARD_LEGACY_NL80211_STA_EVENTS := true
-endif
-
 #QCom Display
 BOARD_EGL_CFG := device/htc/msm7x30-common/egl.cfg
 BOARD_USES_ADRENO_200 := true
