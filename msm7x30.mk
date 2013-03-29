3dcb70cc27cfcd3f639e86f906702ea141dc8ad5
synergydev <synergy@spentlemon.com>
2013-03-29 22:06:40 +0000

    use sourcebuilt libcameraservice

--- msm7x30.mk
+++ msm7x30.mk
@@ -50,8 +50,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_COPY_FILES += \
-    device/htc/msm7x30-common/prebuilt/libsurfaceflinger_client.so:system/lib/libsurfaceflinger_client.so \
-    device/htc/msm7x30-common/prebuilt/libcameraservice.so:system/lib/libcameraservice.so
+    device/htc/msm7x30-common/prebuilt/libsurfaceflinger_client.so:system/lib/libsurfaceflinger_client.so
 
 # hostapd prebuilts
 PRODUCT_COPY_FILES += \
