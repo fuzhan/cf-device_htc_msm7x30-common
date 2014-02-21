27443456ca1a95b2ffef5007844c27bfbd9cf60f
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-02-21 14:46:16 -0800

    Push wpa_supplicant modules down to the leaf product config.
    Since 558477c0b2a5a7db679c1ad7551d43e6364d1df2 in
    external/wpa_supplicant_8 we don't need those modules if
    WPA_SUPPLICANT_VERSION is not set by the BoardConfig.mk.
    
    Based on:
    https://android.googlesource.com/device/lge/mako/+/821cdef6635d091820cbd429853dd858b6fbae17

--- msm7x30.mk
+++ msm7x30.mk
@@ -77,7 +77,11 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-	libnetcmdiface
+	libnetcmdiface \
+	libwpa_client \
+	hostapd \
+	wpa_supplicant \
+	wpa_supplicant.conf
 
 # HAL
 PRODUCT_PACKAGES += \
